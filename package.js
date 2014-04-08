4ef7ba997600d8d88aa0af430e4ea31509dcb8a4
Kristian Dupont <kristian@kristiandupont.com>
2014-04-08 13:45:44 +0200

    Updated api.exports etc.

--- package.js
+++ package.js
@@ -3,25 +3,7 @@ Package.describe({
 });
 
 var libFiles = [
-        'canvg/rgbcolor.js',
-        'canvg/StackColor.js',
-        'canvg/canvg.js',
-        'geostats/geostats.min.js',
-        'geostats/jenks.util.js',
         'tipsy/tipsy.js',
-        'intersect/intersect.js',
-        'dictionary.js',
-        'colors.js',
-        'ClusteringNodeProvider.js',
-        'nodes.js',
-        'highlights.js',
-        'labels.js',
-        'links.js',
-        'selector.js',
-        'styles.js',
-        'tags.js',
-        'zoomer.js',
-        'd3graph.js',
         'd3treelayout.js',
         'graphvis.js'
     ];
@@ -31,11 +13,16 @@ Package.on_use(function(api, where) {
     api.add_files(libFiles, ["client"]);
 
     if (api.export) {
-        api.export('d3graph');
-        api.export('d3treelayout');
-        api.export('d3colors');
-        api.export('d3color');
-        api.export('d3selector');
+        api.export('GraphVis');
+        api.export('VisNode');
+        api.export('VisLink')
+        api.export('VisCluster');
+
+        api.export('SvgRenderer');
+        api.export('NodeCircle');
+        api.export('LinkLine');
+        api.export('ClusterHull');
+        api.export('LabelText');
     }
 });
 
@@ -46,11 +33,6 @@ Package.on_test(function (api) {
     api.add_files([
         "tests/jquery-simulate/jquery.simulate.js",
         "tests/stubs.js", "tests/helpers.js",
-//        "tests/d3graph-tests.js", 
-//        "tests/d3links-tests.js",
-//        "tests/clusteringnodeprovider-tests.js", 
-//        "tests/clusteringnodeprovider-clustering-tests.js",
-//        "tests/d3zoomer-tests.js",
         "tests/graphvis-tests.js"
     ], ["client"]);
 });
